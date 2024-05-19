ef0539287f969552b93ef71b4c9063b1aad26de8
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-05-19 10:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function fabb(n) {
-	if(n==0) return 0;
-	if(n==1||n==2) return 1;
-	return fabb(n-1)+fabb(n-2);
-}
-function fibonacci(num) {
-	return fabb(num);
-}
 
+function fibonacci(num) {
+	let arr=new Array();
+	arr.push(0);
+	arr.push(1);
+	for(let i=1; i<=num-2; i++){
+		arr.push(arr[i-2]+arr[i-1]);
+	}
+	return arr[num-1];
+}
 module.exports = fibonacci;
